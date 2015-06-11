4668ec7d030422f3064a65d5d3289e9bcbee454c
Alessandro <alessandro@pointfinder.info>
2015-06-11 16:00:41 +0300

    Email Crawler test 0.0

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Created on Jun 11, 2015
+
+@author: nex
+'''
+from ui import UserInterface
+
+
+if __name__ == '__main__':
+    
+    UI = UserInterface()
+    UI.run()
---
+++ ui/ui.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 11, 2015
+
+@author: nex
+'''
+
+class UserInterface:
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def run(self):
+        print("Running")
